23f9e72df4a6bb87890aa9530a5fa0c6d5953050
Tyler Torola <tyler@tjt.codes>
2016-11-07 08:34:57 -0800

    working on dynamic arr

--- dynamic_array.c
+++ dynamic_array.c
@@ -8,6 +8,9 @@ typedef struct {
 	int startIdx;
 } Array;
 
+int idx(Array * array, int i);
+int val(Array * array, int i);
+
 Array * newArray() {
 	Array * array = malloc(sizeof(Array));
 
@@ -24,7 +27,7 @@ void printArray(Array * array) {
 
 	printf("[");
 	while (i < array->length) {
-		printf("%i", *(array->data + i));
+		printf("%i", val(array, i));
 
 		if (i != array->length - 1) {
 			printf(", ");
@@ -35,8 +38,47 @@ void printArray(Array * array) {
 }
 
 void doubleCapacity(Array * array) {
+	int *new = malloc(sizeof(int) * array->capacity * 2);
+
+	int newIdx = 0;
+	int oldIdx = 0;
+	while(newIdx < array->length) {
+		oldIdx = idx(array, newIdx);
+		new[newIdx] = array->data[oldIdx];
+
+		newIdx++;
+	}
+	free(array->data);
+	array->data = new;
 	array->capacity *= 2;
-	array->data = realloc(array->data, sizeof(int) * array->capacity);
+	array->startIdx = 0;
+}
+
+int idx(Array * array, int i) {
+	if (i < 0 && i < -array->length) {
+		return -1;
+	} else if(i >= 0 && i > array->length) {
+		return -1;
+	}
+
+	if (i < 0) {
+		i = (i % array->length) + array->length;
+	}
+
+	return (array->startIdx + i) % array->capacity;
+}
+
+int val(Array *array, int i) {
+	i = idx(array, i);
+	return array->data[i];
+}
+
+void moveStartIndex(Array *array, int diff) {
+	array->startIdx += diff;
+
+	if (array->startIdx < 0) {
+		array->startIdx = array->capacity + array->startIdx;
+	}
 }
 
 void push(Array * array, int val) {
@@ -44,8 +86,45 @@ void push(Array * array, int val) {
 		doubleCapacity(array);
 	}
 
-	int * next = array->data + array->length + array->startIdx;
-	*next = val;
+	int last = idx(array, -1);
+	int next = last + 1;
 
+	array->data[next] = val;
 	array->length++;
+}
+
+int pop(Array * array) {
+	int val = array->data[idx(array, -1)];
+
+	array->length--;
+
+	return val;
+}
+
+void unshift(Array * array, int val) {
+	moveStartIndex(array, -1);
+
+	int first = idx(array, 0);
+	array->data[first] = val;
+	array->length++;
+}
+
+int main() {
+	Array * a = newArray();
+	push(a, 1);
+	push(a, 22);
+	push(a, 32);
+	push(a, 4);
+	push(a, 5);
+	push(a, 3);
+	push(a, 4);
+	push(a, 5);
+	push(a, 4);
+	push(a, 5);
+	push(a, 3);
+	push(a, 4);
+	// unshift(a, 23);
+	// pop(a);
+	printArray(a);
+	return 0;
 }
